ca96ce5b1c6f51b1541694d3ba194c7d1508d6c9
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-05-05 14:40:35 +0200

    Add SwaggerUI 3.48.0

--- src/main/java/eu/europa/ec/dgc/gateway/model/TrustListType.java
+++
@@ -1,9 +0,0 @@
-package eu.europa.ec.dgc.gateway.model;
-
-public enum TrustListType {
-
-    DSC,
-    UPLOAD,
-    CSCA,
-    AUTHENTICATION
-}
--- src/main/java/eu/europa/ec/dgc/gateway/repository/AuditEventRepository.java
+++
@@ -1,7 +0,0 @@
-package eu.europa.ec.dgc.gateway.repository;
-
-import eu.europa.ec.dgc.gateway.entity.AuditEventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditEventRepository extends JpaRepository<AuditEventEntity, Long> {
-}
