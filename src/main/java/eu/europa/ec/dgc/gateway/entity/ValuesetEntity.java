bd3e28096690f79df3ebe539b41798077222ee2a
Felix Dittrich <Felix.Dittrich@t-systems.com>
2021-06-16 09:01:29 +0200

    Add Unit Tests

--- src/main/java/eu/europa/ec/dgc/gateway/entity/ValuesetEntity.java
+++ src/main/java/eu/europa/ec/dgc/gateway/entity/ValuesetEntity.java
@@ -24,13 +24,17 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "valueset")
+@AllArgsConstructor
+@NoArgsConstructor
 public class ValuesetEntity {
 
     @Id
