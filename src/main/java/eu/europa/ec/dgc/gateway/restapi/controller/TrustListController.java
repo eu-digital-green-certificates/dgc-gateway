b31ad6641a69a957d0f9a12c26af321d0a908a22
Hong Mu <100763593+hhmu@users.noreply.github.com>
2022-03-23 17:03:14 +0100

    Update src/main/java/eu/europa/ec/dgc/gateway/restapi/controller/TrustListController.java
    Co-authored-by: Felix Dittrich <31076102+f11h@users.noreply.github.com>

--- src/main/java/eu/europa/ec/dgc/gateway/restapi/controller/TrustListController.java
+++ src/main/java/eu/europa/ec/dgc/gateway/restapi/controller/TrustListController.java
@@ -233,7 +233,7 @@ public class TrustListController {
         })
     public ResponseEntity<List<TrustListDto>> downloadTrustListFilteredByType(
         @Valid @PathVariable("type") CertificateTypeDto type,
-        @RequestHeader(value = IF_MODIFIED_SINCE_HEADER, required = false) String ifModifiedSince,
+        @RequestHeader(value = HttpHeaders.IF_MODIFIED_SINCE, required = false) ZonedDateTime ifModifiedSince,
         @RequestParam(value = "page", required = false) Integer page,
         @RequestParam(value = "pagesize", required = false) Integer size,
         @RequestAttribute(CertificateAuthenticationFilter.REQUEST_PROP_COUNTRY) String downloaderCountryCode
