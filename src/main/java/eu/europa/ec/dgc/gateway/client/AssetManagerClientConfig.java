f3271136c1d97e37fc99d81800a0dd94331c0302
Felix Dittrich <Felix.Dittrich@t-systems.com>
2022-11-22 15:10:10 +0100

    Fix File Header

--- src/main/java/eu/europa/ec/dgc/gateway/client/AssetManagerClientConfig.java
+++ src/main/java/eu/europa/ec/dgc/gateway/client/AssetManagerClientConfig.java
@@ -1,8 +1,8 @@
 /*-
  * ---license-start
- * EU Digital Green Certificate Gateway Service / dgc-gateway
+ * WHO Digital Documentation Covid Certificate Gateway Service / ddcc-gateway
  * ---
- * Copyright (C) 2021 - 2022 T-Systems International GmbH and all other contributors
+ * Copyright (C) 2022 T-Systems International GmbH and all other contributors
  * ---
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
